709f1267f263dfc4a4f37081822e2bd9bb2ef225
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-06-16 02:09:18 +0900

    blogs post

--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
-from blogs.models import Blog
+from blogs.models import Blog, Category
 # Create your views here.
 
 def post_by_category(request, category_id ):
 
     posts = Blog.objects.filter( status='Published', category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        # redirect the user to homepage
+        return redirect('home')
+    
     context = {
         'posts':posts,
+        'category':category,
     }
     return render(request, 'post_by_category.html', context)
