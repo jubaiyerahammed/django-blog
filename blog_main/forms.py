e79dd6ed83eb9795aeb0b635006ae6a0e04d4f94
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-06-24 07:29:15 +0900

    register

--- blog_main/forms.py
+++ blog_main/forms.py
@@ -5,4 +5,4 @@ from django.contrib.auth.forms import UserCreationForm
 class RegistrationForm(UserCreationForm):
     class Meta:
         model=User
-        fields=( 'username', 'password1', 'password2')
+        fields=('email', 'username', 'password1', 'password2')
--- blog_main/settings.py
+++ blog_main/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
-    'about'
+    'about',
+    'crispy_forms',
+    'crispy_bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -134,3 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
