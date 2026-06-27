9a56e035e66f026db26e0f0ce55a4411f441534a
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-06-27 09:37:53 +0900

    register form done

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import RegistrationForm
 from about.models import About
@@ -32,7 +32,16 @@ def search(request):
     return render(request, 'search.html', context)
 
 def register (request):
-    form= RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+
+    else:
+        form= RegistrationForm()
     context={
         'form':form
     }
